5671580e22772fcfae676ca9226e42161717529a
Nik <cvkeepervc@mail.ru>
2020-07-18 20:24:05 +0300

    patches

--- src/test/java/ru/netology/manager/PosterManagerTest2.java
+++ src/test/java/ru/netology/manager/PosterManagerTest2.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class PosterManagerTest {
+class PosterManagerTest2 {
     @Mock
     FilmRepository repository;
 
